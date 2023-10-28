24f49aa970fc30906f2819ff5b2636174e0b2e15
Naufal Ichsan <naufalichsan@Naufals-MacBook-Pro.local>
2023-10-29 00:27:48 +0700

    add ajax for filtering book

--- books/views.py
+++ books/views.py
@@ -3,36 +3,51 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from itertools import chain
 from . import models
 from . import forms
-
-def get_books(request):
-    data = models.Book.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
 
 
 def show_books(request):
     books = models.Book.objects.all()
+    user_books = models.UserBook.objects.all()
+    data = list(chain(books, user_books))
     context = {
-        'books': books
+        'books': data
     }
     return render(request, "bookpage.html", context)
 
 def search_books(request):
     search = forms.SearchForm(request.GET)
-    books = []
+    search_filter = request.GET.get('filter', 'all')
+    
     if search.is_valid():
         search_term = search.cleaned_data['q']
-        #filter_type = request.GET.get("filter")
-        books = models.Book.objects.filter(title__contains = search_term) | models.Book.objects.filter(authors__contains = search_term) | \
-                models.Book.objects.filter(publisher__contains = search_term) | models.Book.objects.filter(pub_year__contains = search_term)
-                   
-    context = {
-        'books': books,
-        'search': search,
-    }
-    return render(request, "bookpage.html", context)
+        data = []
+
+        if search_filter == 'all':
+            books = models.Book.objects.filter(title__contains = search_term) | models.Book.objects.filter(authors__contains = search_term) | \
+                    models.Book.objects.filter(publisher__contains = search_term)
+            user_books = models.UserBook.objects.filter(title__contains = search_term) | models.UserBook.objects.filter(authors__contains = search_term) | \
+                        models.UserBook.objects.filter(publisher__contains = search_term)
+            data = list(chain(books, user_books))
+
+        elif search_filter == 'title':
+            books = models.Book.objects.filter(title__contains = search_term)
+            user_books = models.UserBook.objects.filter(title__contains = search_term)
+            data = list(chain(books, user_books))
+
+        elif search_filter == 'author':
+            books = models.Book.objects.filter(authors__contains = search_term)
+            user_books = models.UserBook.objects.filter(authors__contains = search_term)
+            data = list(chain(books, user_books))
+
+        elif search_filter == 'publisher':
+            books = models.Book.objects.filter(publisher__contains = search_term)
+            user_books =  models.UserBook.objects.filter(publisher__contains = search_term)
+            data = list(chain(books, user_books))
+
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
 
 @login_required(login_url='main:login')
 def add_books(request):
@@ -48,3 +63,8 @@ def add_books(request):
         'form': form,
     }
     return render(request, "add_book.html", context)
+
+def get_books(request):
+    data = models.Book.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
