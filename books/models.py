7caaf60f84151c568ccc724424044966390d4070
Naufal Ichsan <naufalichsan@Naufals-MacBook-Pro.local>
2023-10-28 18:07:02 +0700

    Update search bar in book and user add book form

--- books/models.py
+++ books/models.py
@@ -16,3 +16,20 @@ class Book(models.Model):
     class Meta:
         ordering = ('title',) #order berdasarkan lexico nama
         verbose_name_plural = 'Books' #mengganti nama plural
+
+class UserBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    authors = models.CharField(max_length=255, null=True, blank=True)
+    publisher = models.CharField(max_length=255, null=True, blank=True)
+    pub_year = models.IntegerField(null=True, blank=True)
+    isbn = models.CharField(max_length=255, null=True, blank=True)
+    cover_img = models.ImageField(upload_to="cover_img/") 
+    date_added = models.DateField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.title
+    # Untuk mengatur metadata pada web admin
+    class Meta:
+        ordering = ('title',) #order berdasarkan lexico nama
+        verbose_name_plural = 'UserBooks' #mengganti nama plural
