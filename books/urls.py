906dac5274cc03201aa80f67b7090fbb69e87671
Naufal Ichsan <naufalichsan@Naufals-MacBook-Pro.local>
2023-10-28 17:53:27 +0700

    update

--- books/urls.py
+++ books/urls.py
@@ -6,4 +6,6 @@ app_name = 'books'
 urlpatterns = [
     path("", views.show_books, name='show-books'),
     path("json/", views.get_books, name="get-books"),
+    path("add-books/", views.add_books, name="add-books"),
+    
 ]
