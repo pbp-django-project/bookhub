97d8311be25b560da851fa57bd4e9e1e298391aa
nano <marianosenduk@gmail.com>
2023-10-29 22:13:01 +0700

    update reviews and books(link the book to the reviews page)

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from reviews.views import show_reviews
 
 app_name = 'books'
 
@@ -8,5 +9,5 @@ urlpatterns = [
     path("json/", views.get_books, name="get-books"),
     path("add-books/", views.add_books, name="add-books"),
      path("search/", views.search_books, name='search-books'),
-    
+    path("reviews/<int:book_id>", show_reviews, name='show-reviews'),
 ]
