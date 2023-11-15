1142eb113449a720f705dd873ccf6bfb35bb7a91
Naufal Ichsan <naufalichsan@Naufals-MacBook-Pro.local>
2023-11-16 00:02:49 +0700

    railway

--- bookhub/settings.py
+++ bookhub/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
 if PRODUCTION:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'railway',
             'USER': 'postgres',
             'PASSWORD': '6FFCfdEc33eBB*GG5*FDADc35gDb4b6d',
